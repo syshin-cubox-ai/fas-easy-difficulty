1a4036fbc0213cc44bcbd0155de3a3247cecc8e9
신석용 <syshin@cubox.ai>
2023-05-04 11:14:47 +0900

    scrfd 코드를 techblog 코드로 변경

--- scrfd.py
+++ scrfd.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import onnxruntime
 
-from utils import clip_coords, resize_preserving_aspect_ratio
+import utils
 
 
 class SCRFD:
@@ -25,47 +25,29 @@ class SCRFD:
         self.conf_thres = conf_thres
         self.iou_thres = iou_thres
 
-        self.mode = None
-        self.session = None
-        self.input_name = None
-        self.request = None
-        if os.path.splitext(model_path)[-1] == '.onnx':
-            self.mode = 'onnx'
-            if device == 'cpu':
-                providers = ['CPUExecutionProvider']
-            elif device == 'cuda':
-                providers = ['CUDAExecutionProvider']
-            elif device == 'openvino':
-                providers = ['OpenVINOExecutionProvider']
-            elif device == 'tensorrt':
-                providers = ['TensorrtExecutionProvider']
-            else:
-                raise ValueError(f'device is invalid: {device}')
-            self.session = onnxruntime.InferenceSession(model_path, providers=providers)
-            session_input = self.session.get_inputs()[0]
-            assert session_input.shape[2] == session_input.shape[3], 'The input shape must be square.'
-            self.img_size = session_input.shape[2]
-            self.input_name = session_input.name
-
-        elif os.path.splitext(model_path)[-1] == '.xml':
-            self.mode = 'openvino'
-            import openvino.runtime
-            core = openvino.runtime.Core()
-            compiled_model = core.compile_model(model_path, 'CPU')
-            self.request = compiled_model.create_infer_request()
-            input_shape = self.request.inputs[0].shape
-            assert input_shape[2] == input_shape[3], 'The input shape must be square.'
-            self.img_size = input_shape[2]
+        if device == 'cpu':
+            providers = ['CPUExecutionProvider']
+        elif device == 'cuda':
+            providers = ['CUDAExecutionProvider']
+        elif device == 'openvino':
+            providers = ['OpenVINOExecutionProvider']
+        elif device == 'tensorrt':
+            providers = ['TensorrtExecutionProvider']
         else:
-            raise ValueError(f'Wrong file extension: {os.path.splitext(model_path)[-1]}')
+            raise ValueError(f'device is invalid: {device}')
+        self.session = onnxruntime.InferenceSession(model_path, providers=providers)
+        session_input = self.session.get_inputs()[0]
+        assert session_input.shape[2] == session_input.shape[3], 'The input shape must be square.'
+        self.img_size = session_input.shape[2]
+        self.input_name = session_input.name
 
-    def _transform_image(self, img: np.ndarray, scale_ratio=1.0) -> Tuple[np.ndarray, float]:
+    def _transform_image(self, img: np.ndarray) -> Tuple[np.ndarray, float]:
         """
         Resizes the input image to fit img_size while preserving aspect ratio.
         This performs BGR to RGB, HWC to CHW, normalization, and adding batch dimension.
         (mean=(127.5, 127.5, 127.5), std=(128.0, 128.0, 128.0))
         """
-        img, scale = resize_preserving_aspect_ratio(img, self.img_size, scale_ratio)
+        img, scale = utils.resize_preserving_aspect_ratio(img, self.img_size)
 
         pad = (0, self.img_size - img.shape[0], 0, self.img_size - img.shape[1])
         img = cv2.copyMakeBorder(img, *pad, cv2.BORDER_CONSTANT, value=(0, 0, 0))
@@ -112,29 +94,6 @@ class SCRFD:
         pred = pred[keep, :]
         return pred
 
-    def _inference(self, img: np.ndarray) -> np.ndarray:
-        if self.mode == 'onnx':
-            pred = self.session.run(None, {self.input_name: img})[0]
-        elif self.mode == 'openvino':
-            pred = self.request.infer({0: img}).popitem()[1]
-        else:
-            raise ValueError(f'Wrong mode: {self.mode}')
-        pred = self._non_max_suppression(pred)
-        return pred
-
-    def _padded_detect_one(self, img: np.ndarray) -> Optional[np.ndarray]:
-        original_img_shape = img.shape[:2]
-        transformed_img, scale = self._transform_image(img, scale_ratio=1.5)
-        pred = self._inference(transformed_img)
-        if pred.shape[0] > 0:
-            # Rescale coordinates from inference size to input image size
-            pred[:, :4] /= scale
-            pred[:, 5:] /= scale
-            pred = clip_coords(pred, original_img_shape)
-            return pred
-        else:
-            return None
-
     def detect_one(self, img: np.ndarray) -> Optional[np.ndarray]:
         """
         Perform face detection on a single image.
@@ -143,26 +102,27 @@ class SCRFD:
         Return:
             pred:
                 Post-processed prediction. Shape=(number of faces, 15)
-                15 is composed of bbox coordinates(4), object confidence(1), and keypoints coordinates(10).
-                The coordinate format is x1y1x2y2 (bbox), xy per point (keypoints).
+                15 is composed of bbox coordinates(4), object confidence(1), and landmarks coordinates(10).
+                The coordinate format is x1y1x2y2 (bbox), xy per point (landmarks).
                 The unit is image pixel.
                 If no face is detected, output None.
         """
         original_img_shape = img.shape[:2]
-        transformed_img, scale = self._transform_image(img)
-        pred = self._inference(transformed_img)
+        img, scale = self._transform_image(img)
+        pred = self.session.run(None, {self.input_name: img})[0]
+        pred = self._non_max_suppression(pred)
         if pred.shape[0] > 0:
             # Rescale coordinates from inference size to input image size
             pred[:, :4] /= scale
             pred[:, 5:] /= scale
-            pred = clip_coords(pred, original_img_shape)
+            pred = utils.clip_coords(pred, original_img_shape)
             return pred
         else:
             return None
 
     def parse_prediction(self, pred: np.ndarray) -> Tuple[List, List, List]:
-        """Parse prediction to bbox, confidence, and keypoints."""
+        """Parse prediction to bbox, confidence, and landmarks."""
         bbox = pred[:, :4].round().astype(np.int32).tolist()
         conf = pred[:, 4].tolist()
-        kps = pred[:, 5:].round().astype(np.int32).tolist()
-        return bbox, conf, kps
+        landmarks = pred[:, 5:].round().astype(np.int32).tolist()
+        return bbox, conf, landmarks
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple
+from typing import Tuple
 
 import cv2
 import numpy as np
@@ -15,8 +15,7 @@ def resize_preserving_aspect_ratio(img: np.ndarray, img_size: int, scale_ratio=1
     return img, scale
 
 
-def clip_coords(boxes: Union[torch.Tensor, np.ndarray], shape: tuple) -> Union[torch.Tensor, np.ndarray]:
-    # MODIFIED for face detection
+def clip_coords(boxes: np.ndarray, shape: tuple) -> np.ndarray:
     # Clip bounding xyxy bounding boxes to image shape (height, width)
     boxes[:, 0] = boxes[:, 0].clip(0, shape[1])  # x1
     boxes[:, 1] = boxes[:, 1].clip(0, shape[0])  # y1
@@ -27,20 +26,30 @@ def clip_coords(boxes: Union[torch.Tensor, np.ndarray], shape: tuple) -> Union[t
     return boxes
 
 
-def draw_prediction(img: np.ndarray, bbox: list, conf: list, kps: list = None, thickness=2, hide_conf=False):
-    # Draw prediction on the image. If the keypoints is None, only draw the bbox.
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+
+def draw_prediction(img: np.ndarray, bbox: list, conf: list, landmarks: list = None, thickness=2, hide_conf=False):
+    # Draw prediction on the image. If the landmarks is None, only draw the bbox.
     assert img.ndim == 3, f'img dimension is invalid: {img.ndim}'
     assert img.dtype == np.uint8, f'img dtype must be uint8, got {img.dtype}'
     assert img.shape[-1] == 3, 'Pass BGR images. Other Image formats are not supported.'
     assert len(bbox) == len(conf), 'bbox and conf must be equal length.'
-    if kps is None:
-        kps = [None] * len(bbox)
-    assert len(bbox) == len(conf) == len(kps), 'bbox, conf, and kps must be equal length.'
+    if landmarks is None:
+        landmarks = [None] * len(bbox)
+    assert len(bbox) == len(conf) == len(landmarks), 'bbox, conf, and landmarks must be equal length.'
 
     bbox_color = (0, 255, 0)
     conf_color = (0, 255, 0)
-    kps_colors = ((0, 165, 255), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255))
-    for bbox_one, conf_one, kps_one in zip(bbox, conf, kps):
+    landmarks_colors = ((0, 165, 255), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255))
+    for bbox_one, conf_one, landmarks_one in zip(bbox, conf, landmarks):
         # Draw bbox
         x1, y1, x2, y2 = bbox_one
         cv2.rectangle(img, (x1, y1), (x2, y2), bbox_color, thickness, cv2.LINE_AA)
@@ -49,7 +58,7 @@ def draw_prediction(img: np.ndarray, bbox: list, conf: list, kps: list = None, t
         if not hide_conf:
             cv2.putText(img, f'{conf_one:.2f}', (x1, y1 - 2), None, 0.6, conf_color, thickness, cv2.LINE_AA)
 
-        # Draw keypoints
-        if kps_one is not None:
-            for point_x, point_y, color in zip(kps_one[::2], kps_one[1::2], kps_colors):
+        # Draw landmarks
+        if landmarks_one is not None:
+            for point_x, point_y, color in zip(landmarks_one[::2], landmarks_one[1::2], landmarks_colors):
                 cv2.circle(img, (point_x, point_y), 2, color, cv2.FILLED)
