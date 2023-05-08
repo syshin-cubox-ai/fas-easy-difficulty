0d2096379d18acb1a87405d2c4bce0429f617b1a
신석용 <syshin@cubox.ai>
2023-05-08 17:41:33 +0900

    Remove pytorch dependency

--- predict_webcam.cmd
+++
@@ -1,3 +0,0 @@
-call conda activate ldc
-call yolo predict model=torch_files/yolov8n-smartphone.pt source=0 show=True line_thickness=2 conf=0.6
-pause
--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ from typing import Tuple
 
 import cv2
 import numpy as np
-import torch
 
 
 def resize_preserving_aspect_ratio(img: np.ndarray, img_size: int, scale_ratio=1.0) -> Tuple[np.ndarray, float]:
@@ -28,7 +27,7 @@ def clip_coords(boxes: np.ndarray, shape: tuple) -> np.ndarray:
 
 def xywh2xyxy(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y = np.copy(x)
     y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
     y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
     y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
@@ -38,7 +37,7 @@ def xywh2xyxy(x):
 
 def xywh2xywh(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, w, h] where xy1=top-left, xy2=bottom-right
-    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y = np.copy(x)
     y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
     y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
     return y
