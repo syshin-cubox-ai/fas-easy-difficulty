038e7b420deb7f422564fceb45252b5609d758d7
신석용 <syshin@cubox.ai>
2024-04-18 17:50:53 +0900

    눈 깜빡임 감지 코드 mediapipe로 재구성

--- eye_blink_detector_mediapipe.py
+++ eye_blink_detector_mediapipe.py
@@ -2,22 +2,27 @@ import platform
 import time
 
 import cv2
+import mediapipe as mp
 import numpy as np
-from spiga.demo.visualize.plotter import Plotter
-from spiga.inference.config import ModelConfig
-from spiga.inference.framework import SPIGAFramework
-from ultralytics import YOLO
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 import utils
 
 
 class EyeBlinkDetector:
-    def __init__(self):
+    def __init__(self, device="cpu"):
         # Settings
-        self.face_detector = YOLO("onnx_files/yolov8n-faceperson.onnx", "pose")
-        self.face_aligner = SPIGAFramework(ModelConfig("wflw"))
-        self.face_detector_conf = 0.35
-        self.face_detector_iou = 0.7
+        base_options = python.BaseOptions(
+            model_asset_path="mediapipe_files/face_landmarker.task",
+            delegate=python.BaseOptions.Delegate.CPU if device == "cpu" else python.BaseOptions.Delegate.GPU,
+        )
+        self.mp_detector = vision.FaceLandmarker.create_from_options(
+            vision.FaceLandmarkerOptions(
+                base_options=base_options,
+                num_faces=1,
+            )
+        )
         self.ear_thres = 0.2
         self.ear_continuous_frames = 2
 
@@ -29,23 +34,19 @@ class EyeBlinkDetector:
         start = time.perf_counter()
         is_blinked = False
 
-        # 얼굴 검출
-        r = self.face_detector.predict(img, conf=self.face_detector_conf, iou=self.face_detector_iou,
-                                       device="cuda", classes=[0])[0]
-        if len(r) > 0:
-            # 가장 큰 얼굴 하나만 남기기
-            bbox = self.leave_max_area_item(r.boxes.xyxy.cpu().numpy())
+        # 얼굴 랜드마크 검출
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        result = self.mp_detector.detect(mp_image)
 
-            # 얼굴 랜드마크 검출
-            features = self.face_aligner.inference(img, utils.xyxy2xywh(bbox.copy()))
-
+        if len(result.face_landmarks) > 0:
             # 형식 변환
-            bbox = bbox.squeeze(0).round().astype(np.int32)
-            landmarks = np.array(features["landmarks"]).squeeze(0)
+            landmarks = np.array(result.face_landmarks[0])
+            left_eye = landmarks[[33, 160, 159, 158, 133, 153, 145, 144]]
+            right_eye = landmarks[[362, 385, 386, 387, 263, 373, 374, 380]]
+            left_eye = np.array([[int(round(i.x * img.shape[1])), int(round(i.y * img.shape[0]))] for i in left_eye])
+            right_eye = np.array([[int(round(i.x * img.shape[1])), int(round(i.y * img.shape[0]))] for i in right_eye])
 
             # Eye Aspect Ratio 계산
-            left_eye = landmarks[60:68]
-            right_eye = landmarks[68:76]
             left_eye_ear = self.compute_eye_aspect_ratio(left_eye)
             right_eye_ear = self.compute_eye_aspect_ratio(right_eye)
             ear = (left_eye_ear + right_eye_ear) / 2
@@ -60,22 +61,14 @@ class EyeBlinkDetector:
                 self._counter = 0
             fps = 1 / (time.perf_counter() - start)
 
-            # Plot (bbox, labels, landmarks, fps)
+            # Plot (labels, landmarks, fps)
             if plot:
-                x1, y1, x2, y2 = bbox
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 255), 1, cv2.LINE_AA)
-                utils.put_border_text(img, f"ear={ear:.3f} blinks={self._number_blinks}", (x1, y1 - 2), 0.56)
-                img = Plotter().landmarks.draw_landmarks(img, landmarks[60:76], thick=1)
+                utils.put_border_text(img, f"ear={ear:.3f} blinks={self._number_blinks}", (10, 46), 0.56)
+                for xy in np.concatenate([left_eye, right_eye], 0):
+                    cv2.circle(img, (xy[0], xy[1]), 1, (0, 255, 0), -1)
                 utils.put_border_text(img, f"{fps:.2f} FPS", (10, 26), 0.6)
 
         return is_blinked, self._number_blinks
-
-    @staticmethod
-    def leave_max_area_item(pred: np.ndarray) -> np.ndarray:
-        area = (pred[..., 2] - pred[..., 0]) * (pred[..., 3] - pred[..., 1])
-        max_idx = np.argmax(area)
-        pred = np.expand_dims(pred[max_idx], 0)
-        return pred
 
     @staticmethod
     def compute_eye_aspect_ratio(eye: np.ndarray) -> float:
